2a98c39a510ce98b8983ed7478c098c0115c7d07
Ravi Tokas <ravitokas90@gmail.com>
2024-06-12 17:49:40 +0530

    add hero component

---
+++ src/components/hero.tsx
@@ -0,0 +1,12 @@
+import type { StaticImageData } from "next/image";
+import Image from "next/image";
+
+interface HeroProps {
+  imgData: StaticImageData;
+  imgAlt: string;
+  title: string;
+}
+
+export default function Hero(props: HeroProps) {
+  return <div></div>;
+}
